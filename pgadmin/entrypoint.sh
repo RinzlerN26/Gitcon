74d200dcbb9edef0241eb6fc7fc355774458d31e
RinzlerN26 <nishantonnet45@gmail.com>
2026-08-25 20:12:39 +0530

    Change Add Server Config for pg admin.

--- pgadmin/entrypoint.sh
+++ pgadmin/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+echo "===== Generating pgAdmin servers.json ====="
+
 envsubst < /pgadmin4/servers.json.template > /pgadmin4/servers.json
+
+echo "===== Generated servers.json ====="
+cat /pgadmin4/servers.json
+echo "====================================="
 
 exec /entrypoint.sh "$@"
